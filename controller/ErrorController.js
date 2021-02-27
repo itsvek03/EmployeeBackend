14bbeac15369f1136f3b2c6286ed0b7a4f72850f
MAURYA VIVEK <vivekmaurya3m9@gmail.com>
2021-02-27 14:18:35 +0530

    ErrorChanges

--- controller/ErrorController.js
+++ controller/ErrorController.js
@@ -61,7 +61,7 @@ module.exports = (err, req, res, next) => {
         let error = {
             ...err,
         };
-        if (error.name === "CastError") error = handleCastErrorDB(error);
+        //if (error.name === "CastError") error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
         if (error.name === "ValidationError")
             error = handleValidationErrorDB(error);
